f53524d269b4a9a2472cac04b38a117005916756
Tarek Ziade <tarek@ziade.org>
2015-10-20 08:30:40 +0200

    initial steps

--- udun/__init__.py
+++ udun/__init__.py
@@ -1,4 +1,22 @@
 
+def _get_redis_events():
+    return []
+
+
+def _get_impacted_collections(events):
+    return []
+
+
+def _poke_balrog(collection_ids):
+    pass
+
 
 def main():
-    pass
+    # 1. grab things in the redis list
+    events = _get_redis_events()
+
+    # 2. combine per-collection
+    collection_ids = _get_impacted_collections(events)
+
+    # 3. send it to Balrog
+    _poke_balrog(collection_ids)
